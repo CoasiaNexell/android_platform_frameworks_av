3c827b37a35a8ff69c12403865f5c3cc31debfcb
HeynChul Jun <hcjun@coasianexell.com>
2020-10-22 13:27:25 +0900

    framework_av:NuPlayerRenderer2: handle fractional audio data before EOS
    This patch was taken from android10.
    When audio reaches EOS and MediaClock has not started, use video
     timestamp to start MediaClock.
    
    Signed-off-by: HeynChul Jun <hcjun@coasianexell.com>
    Change-Id: I450da240dc9e377ac2d675515db8edebc78196c6

--- media/libmediaplayer2/nuplayer2/NuPlayer2Renderer.cpp
+++ media/libmediaplayer2/nuplayer2/NuPlayer2Renderer.cpp
@@ -1415,9 +1415,15 @@ void NuPlayer2::Renderer::notifyEOS_l(bool audio, status_t finalResult, int64_t 
         mHasAudio = false;
         if (mNextVideoTimeMediaUs >= 0) {
             int64_t mediaUs = 0;
-            mMediaClock->getMediaTime(ALooper::GetNowUs(), &mediaUs);
-            if (mNextVideoTimeMediaUs > mediaUs) {
-                mMediaClock->updateMaxTimeMedia(mNextVideoTimeMediaUs);
+            int64_t nowUs = ALooper::GetNowUs();
+            status_t result = mMediaClock->getMediaTime(nowUs, &mediaUs);
+            if (result == OK) {
+                if (mNextVideoTimeMediaUs > mediaUs) {
+                    mMediaClock->updateMaxTimeMedia(mNextVideoTimeMediaUs);
+                }
+            } else {
+                mMediaClock->updateAnchor(
+                        mNextVideoTimeMediaUs, nowUs, mNextVideoTimeMediaUs + 100000);
             }
         }
     }
